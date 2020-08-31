de36d60cd578c1e4a628ecee6a2359c5b0280558
Mahmoud Mousa Hamad <mahmoud@Landons-MacBook-Pro-768.local>
2020-08-31 13:11:10 -0500

    Cleaned up the code.

--- src/View.jsx
+++ src/View.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Bell from './components/Bell';
 
 import './View.css';
 
 export default class NotificationsView extends React.Component {
 	render () {
 		return (
-			<React.Fragment>
-				<div>Notifications</div>
-				<Bell />
-			</React.Fragment>
+			<div></div>
 		);
 	}
 }
