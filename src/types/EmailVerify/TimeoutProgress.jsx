85f66587e61c2917f8ae1ed68989d03838bf507f
Jonathan Grimes <jsg2021@gmail.com>
2020-11-24 12:58:29 -0600

    NTI-9933 - Fix status bar & notification position

--- src/types/EmailVerify/TimeoutProgress.jsx
+++ src/types/EmailVerify/TimeoutProgress.jsx
@@ -5,10 +5,10 @@ const styles = css`
 	.container {
 		background-color: var(--panel-background-alt);
 		position: absolute;
-		right: 95px;
-		width: 360px;
+		left: 0;
+		right: 0;
+		bottom: 0;
 		height: 6px;
-		top: calc(80px + 24px - 6px);
 		overflow: hidden;
 		border-bottom-left-radius: 4px;
 		border-bottom-right-radius: 4px;
