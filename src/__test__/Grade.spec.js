a8d74035ac25ef478ca59375e609527d110a5bde
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2020-12-30 14:12:27 +0200

    Grade notification type unit test

---
+++ src/__test__/Grade.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+
+import React from 'react';
+import {render, waitFor} from '@testing-library/react';
+
+import Grade from '../types/Grade';
+
+describe('Test Grade Notification Type', () => {
+	test('Creator is system', () => {
+	});
+});
