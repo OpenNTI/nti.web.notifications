603c3e1bf375aaf9c2133fb975e1d68814a4b791
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2020-12-30 14:12:19 +0200

    Feedback notification type unit test

---
+++ src/__test__/Feedback.spec.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+
+import React from 'react';
+import {render, waitFor} from '@testing-library/react';
+
+import Feedback from '../types/Feedback';
+
+describe('Feedback Notification Type', () => {
+	test('Feedback item with null assignment', async () => {
+		const item = {
+			Item: {
+				MimeType: 'assessment.userscourseassignmenthistoryitemfeedback',
+				title: 'This is the assignment title'
+			},
+			getLastModified: () => Date.now(),
+		};
+
+		const {getByText} = render(<Feedback item={item}></Feedback>);
+
+		expect(await waitFor(() => getByText('posted feedback on This is the assignment title'))).toBeTruthy();
+	});
+});
