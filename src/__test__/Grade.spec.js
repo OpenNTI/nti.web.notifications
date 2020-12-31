d056c23e3a5c1eedb2191d75bdf83e1d2789cd70
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2020-12-31 14:13:37 +0200

    Grade notification type uint test

--- src/__test__/Grade.spec.js
+++ src/__test__/Grade.spec.js
@@ -1,11 +1,31 @@
 /* eslint-env jest */
-
 import React from 'react';
-import {render, waitFor} from '@testing-library/react';
+import {render} from '@testing-library/react';
+import {Hooks, Presentation} from '@nti/web-commons';
 
 import Grade from '../types/Grade';
 
 describe('Test Grade Notification Type', () => {
 	test('Creator is system', () => {
+		const item = {
+			Item: {
+				creator: 'system',
+				AssignmentName: 'Assignment Name',
+				CourseName: 'Course Name',
+				MimeType: 'grade',
+				CatalogEntry: 'test',
+			},
+			getLastModified: () => Date.now(),
+		};
+
+		jest.spyOn(Hooks, 'useChanges').mockImplementation(() => {});
+
+		jest.spyOn(Presentation, 'Asset').mockImplementation(() => (<div className="icon">Icon</div>));
+
+		const element = render(<Grade item={item}></Grade>);
+
+		expect(element.queryByTestId('grade-creator-icon')).toBeTruthy();
+
+		expect(element.queryByTestId('grade-course-name')).toBeTruthy();
 	});
 });
