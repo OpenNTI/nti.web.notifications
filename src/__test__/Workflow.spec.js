36f81ea2ac0ff080ce9b815e55547bd8796d2666
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2021-01-04 13:49:01 +0200

    Mock getAppUser for Verify Dialog

--- src/__test__/Workflow.spec.js
+++ src/__test__/Workflow.spec.js
@@ -1,11 +1,14 @@
 /* eslint-env jest */
 
 import React from 'react';
-import {render} from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import {render, waitFor} from '@testing-library/react';
 import {StoreContextWrapper} from '@nti/lib-store';
 
 import Store from '../Store';
 import EmailVerificationWorkflow from '../types/EmailVerify/Workflow';
+
+import useMockServer from './utils/use-mock-server';
 
 describe('Test the workflow of email verification', () => {
 	test('Notice is rendered when correct conditions are met', () => {
@@ -38,19 +41,32 @@ describe('Test the workflow of email verification', () => {
 		expect(component.queryByTestId('email-verification-notice')).toBeNull();
 	});
 
-	test('Email verify dialog is rendered when correct conditions are met', () => {
+	test('Email verify dialog is rendered when correct conditions are met', async () => {
+		const getAppUser = jest.fn().mockImplementation(() => {
+			return {
+				email: 'test@test.com'
+			};
+		});
+
+		useMockServer({getAppUser});
+
 		const store = new Store();
 
 		store.set({
 			needsVerification: true,
 			emailVerificationRequested: true,
+			verifiedDate: null,
 		});
 
-		const component = render(
-			<StoreContextWrapper store={store}>
-				<EmailVerificationWorkflow />
-			</StoreContextWrapper>
-		);
+		let component;
+
+		await waitFor(() => {
+			component = render(
+				<StoreContextWrapper store={store}>
+					<EmailVerificationWorkflow />
+				</StoreContextWrapper>
+			);
+		});
 
 		expect(component.queryByTestId('email-verify-dialog')).toBeTruthy();
 
